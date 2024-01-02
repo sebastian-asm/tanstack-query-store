b57f7e86cfe513010364e9a9ba34499dc0e48d09
Sebastian Sanchez <sebastian.asm@outlook.com>
2024-01-02 10:49:03 -0300

    inicio del demo

---
+++ front/src/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import { NextUIProvider } from '@nextui-org/react'
+
+import { router } from './router/router.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <NextUIProvider>
+      <main className="dark text-foreground bg-background">
+        <RouterProvider router={router} />
+      </main>
+    </NextUIProvider>
+  </React.StrictMode>
+)
---
+++ front/src/products/index.ts
@@ -0,0 +1,7 @@
+export { CompleteListPage } from './pages/CompleteListPage'
+export { MensPage } from './pages/MensPage'
+export { NewProduct } from './pages/NewProduct'
+export { ProductCard } from './components/ProductCard'
+export { ProductList } from './components/ProductList'
+export { StoreLayout } from './layout/StoreLayout'
+export { WomensPage } from './pages/WomensPage'
